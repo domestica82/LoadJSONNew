a7c90ead37e844d83d524fdb550b94ec8c8144d0
domes <domesticanandina@gmail.com>
2021-11-12 12:34:57 +0900

    Fixed

--- Sources/LoadJSON/TextStyle.swift
+++ Sources/LoadJSON/TextStyle.swift
@@ -75,7 +75,7 @@ public struct TextStyle: View {
         let cardTitle: Color = colorScheme == .light ? Color.black : Color.white
         let colorMode: Color = thisTag.contains("date") ? Color.secondary : thisTag.contains("supportCard") ? supportCard : thisTag.contains("contact-link") || thisTag.contains("master") ? Color.yellow :
         thisTag.contains("gameContent") ? ColorData.bluegreyStart : thisTag.contains("cardTitle") ? cardTitle :  isLightMode
-//        let edgeSet = contents.isNeedPadding ? changePadding(edgeSet: contents.padding!) : []
+        let edgeSet = contents.isNeedPadding ? changePadding(edgeSet: contents.padding!) : []
         VStack(alignment: .leading){
             if section {
                 Group{

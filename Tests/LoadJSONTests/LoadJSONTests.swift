a5cba6eb0ef42d678aa29e13af0a92d52ac4570c
domes <domesticanandina@gmail.com>
2021-11-12 16:00:07 +0900

    Changed TextStyleView

--- Tests/LoadJSONTests/LoadJSONTests.swift
+++ Tests/LoadJSONTests/LoadJSONTests.swift
@@ -4,10 +4,9 @@ import XCTest
 final class LoadJSONTests: XCTestCase {
     func testExample() throws {
         let item = GeneralContentsModel(id: 2, name: "name", tag: "tag", fontStyle: .body, fontDesign: .default, markDown: false, isNeedBold: false, isNeedPadding: false, padding: [0, 20], scale: 2)
-        let test = TextStyle(contents: item, section: false, alert: false)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(test.contents.name, "name")
+        XCTAssertEqual(item.name, "name")
     }
 }

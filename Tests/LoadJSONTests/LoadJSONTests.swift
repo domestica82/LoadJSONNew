4bb094dc91b0f5907d340cf2593ec0599e9ef5d3
domes <domesticanandina@gmail.com>
2022-11-04 23:30:51 +0900

    Changed README.md

--- Tests/LoadJSONTests/LoadJSONTests.swift
+++ Tests/LoadJSONTests/LoadJSONTests.swift
@@ -3,10 +3,12 @@ import XCTest
 
 final class LoadJSONTests: XCTestCase {
     func testExample() throws {
+        
         let item = GeneralContentsModel(id: 2, name: "name", tag: "tag", fontStyle: .body, fontDesign: .default, markDown: false, isNeedBold: false, isNeedPadding: false, padding: [0, 20], scale: 2)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(item.name, "name")
+        
     }
 }
